d999ba80a7db1b339a4509fd8258a8186d40c9d1
Morten Dahl <douglas.botley@gmail.com>
2014-03-10 17:20:37 +0100

    two iterative solutions

--- src/progin/chapter7_recursion/TelephoneWords.java
+++ src/progin/chapter7_recursion/TelephoneWords.java
@@ -7,31 +7,32 @@ public class TelephoneWords {
 	/*
 	 * Problem: generate all words (sequences) that translate from a string
 	 * 
-	 * Solution: recursively explore tree where a node at depth i has branches
-	 * corresponding to the character at position i - note that all sub-trees
-	 * are identical (suggesting that bottom-up is better -- fewer method calls)
-	 * - similar to combinations, but now the branches are not include/exclude
-	 * but the possible translations
+	 * Solution: recursively explore tree where a node at depth i has branches corresponding to the character at position i 
+	 *  - note that all sub-trees are identical (suggesting that bottom-up is better -- fewer method calls)
+	 *  - similar to combinations, but now the branches are not include/exclude but the possible translations
+	 *  
 	 */
 
 	public static void main(String[] args) {
 
 		List<List<String>> solutions = new ArrayList<List<String>>();
 
-		// String input = "";
-		// String input = "1";
-		// String input = "12";
+		//String input = "";
+		//String input = "1";
+		//String input = "12";
+		//String input = "123";
 		String input = "8662665";
-		solutions.add(translationsBottomUp(input));
-		solutions.add(translationsTopDown(input));
+		solutions.add(translationsRecursiveBottomUp(input));
+		solutions.add(translationsRecursiveTopDown(input));
+		solutions.add(translationIterativeBottomUp(input));
+		solutions.add(translationIterativeTopDown(input));
 
 		for (List<String> solution : solutions) {
 			System.out.print(solution.size() + "  ");
 		}
 		System.out.println();
 
-		// assume they all have the same length; otherwise we may get index out
-		// of bounds exception
+		// assume they all have the same length; otherwise we may get index out of bounds exception
 		for (int i = 0; i < solutions.get(0).size(); i++) {
 			for (int j = 0; j < solutions.size(); j++) {
 				System.out.print("'" + solutions.get(j).get(i) + "'  ");
@@ -42,23 +43,24 @@ public class TelephoneWords {
 	}
 
 	
-	private static char[][] keyMap = { { '0' }, // 0
-			{ '1' }, // 1
-			{ 'a', 'b', 'c' }, // 2
-			{ 'd', 'e', 'f' }, // 3
-			{ 'g', 'h', 'f' }, // 4
-			{ 'j', 'k', 'l' }, // 5
-			{ 'm', 'n', 'o' }, // 6
-			{ 'p', 'r', 's' }, // 7
-			{ 't', 'u', 'v' }, // 8
-			{ 'w', 'x', 'y' } // 9
+	private static char[][] keyMap = { 
+			{ '0' }, 			// 0
+			{ '1' }, 			// 1
+			{ 'a', 'b', 'c' },	// 2
+			{ 'd', 'e', 'f' },	// 3
+			{ 'g', 'h', 'f' },	// 4
+			{ 'j', 'k', 'l' },	// 5
+			{ 'm', 'n', 'o' },	// 6
+			{ 'p', 'r', 's' },	// 7
+			{ 't', 'u', 'v' },	// 8
+			{ 'w', 'x', 'y' }	// 9
 	};
 
 	
 	
 	
 	
-	public static List<String> translationsBottomUp(String input) {
+	public static List<String> translationsRecursiveBottomUp(String input) {
 		List<String> combs = new ArrayList<String>();
 		if (input.length() == 0) {
 			// base case
@@ -66,9 +68,8 @@ public class TelephoneWords {
 		} else {
 			String head = input.substring(0, 1);
 			String tail = input.substring(1);
-			// recursive call may be outside loop since all sub-trees are
-			// identical
-			List<String> tailTrans = translationsBottomUp(tail);
+			// recursive call may be outside loop since all sub-trees are identical
+			List<String> tailTrans = translationsRecursiveBottomUp(tail);
 			for (char headTran : keyMap[Integer.parseInt(head)]) {
 				for (String tailTran : tailTrans) {
 					combs.add(headTran + tailTran);
@@ -77,19 +78,38 @@ public class TelephoneWords {
 		}
 		return combs;
 	}
+	
+	
+	
+	
+	
+	public static List<String> translationIterativeBottomUp(String input) {
+		List<String> trans = new ArrayList<String>();
+		trans.add("");
+		for (int i = input.length()-1; i >= 0; i--) {
+			List<String> newTrans = new ArrayList<String>();
+			String nextHead = input.substring(i, i+1);
+			for (char nextTran : keyMap[Integer.parseInt(nextHead)]) {
+				for (String head : trans) {
+					newTrans.add(nextTran + head);
+				}
+			}
+			trans = newTrans;
+		}
+		return trans;
+	}
 
 	
 	
 	
 	
-	public static List<String> translationsTopDown(String input) {
+	public static List<String> translationsRecursiveTopDown(String input) {
 		List<String> combs = new ArrayList<String>();
-		translationsTopDown(input, "", combs);
+		translationsRecursiveTopDown(input, "", combs);
 		return combs;
 	}
 
-	private static void translationsTopDown(String input, String output,
-			List<String> acc) {
+	private static void translationsRecursiveTopDown(String input, String output, List<String> acc) {
 		if (input.length() == 0) {
 			// base case
 			acc.add(output);
@@ -98,8 +118,29 @@ public class TelephoneWords {
 			String tail = input.substring(1);
 			for (char headTran : keyMap[Integer.parseInt(head)]) {
 				// note that call is now inside loop since 'output' is changing!
-				translationsTopDown(tail, output + headTran, acc);
+				translationsRecursiveTopDown(tail, output + headTran, acc);
 			}
 		}
 	}
+	
+	
+	
+	
+	
+	public static List<String> translationIterativeTopDown(String input) {
+		List<String> trans = new ArrayList<String>();
+		trans.add("");
+		for (int i = 0; i < input.length(); i++) {
+			List<String> newTrans = new ArrayList<String>();
+			String nextHead = input.substring(i, i+1);
+			for (char nextTran : keyMap[Integer.parseInt(nextHead)]) {
+				for (String head : trans) {
+					newTrans.add(head + nextTran);
+				}
+			}
+			trans = newTrans;
+		}
+		return trans;
+	}
+	
 }
