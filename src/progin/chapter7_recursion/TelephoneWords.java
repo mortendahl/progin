4b8ce477d241c1c21a22b6f15f87d6b3b65c5664
Morten Dahl <douglas.botley@gmail.com>
2014-03-10 16:26:00 +0100

    spaces

--- src/progin/chapter7_recursion/TelephoneWords.java
+++ src/progin/chapter7_recursion/TelephoneWords.java
@@ -3,56 +3,57 @@ package progin.chapter7_recursion;
 import java.util.*;
 
 public class TelephoneWords {
-	
+
 	/*
 	 * Problem: generate all words (sequences) that translate from a string
 	 * 
-	 * Solution: recursively explore tree where a node at depth i has branches corresponding to the character at position i
-	 *  - note that all sub-trees are identical (suggesting that bottom-up is better -- fewer method calls)
-	 *  - similar to combinations, but now the branches are not include/exclude but the possible translations   
-	 * 
+	 * Solution: recursively explore tree where a node at depth i has branches
+	 * corresponding to the character at position i - note that all sub-trees
+	 * are identical (suggesting that bottom-up is better -- fewer method calls)
+	 * - similar to combinations, but now the branches are not include/exclude
+	 * but the possible translations
 	 */
 
 	public static void main(String[] args) {
-		
+
 		List<List<String>> solutions = new ArrayList<List<String>>();
-		
-		//String input = "";
-		//String input = "1";
-		//String input = "12";
+
+		// String input = "";
+		// String input = "1";
+		// String input = "12";
 		String input = "8662665";
 		solutions.add(translationsBottomUp(input));
 		solutions.add(translationsTopDown(input));
-		
+
 		for (List<String> solution : solutions) {
 			System.out.print(solution.size() + "  ");
 		}
 		System.out.println();
-		
-		// assume they all have the same length; otherwise we may get index out of bounds exception
+
+		// assume they all have the same length; otherwise we may get index out
+		// of bounds exception
 		for (int i = 0; i < solutions.get(0).size(); i++) {
 			for (int j = 0; j < solutions.size(); j++) {
 				System.out.print("'" + solutions.get(j).get(i) + "'  ");
 			}
 			System.out.println();
 		}
-		
+
 	}
+
 	
-	private static char[][] keyMap = { 	
-									{ '0' }, 			// 0
-									{ '1' },			// 1 
-									{ 'a', 'b', 'c' },	// 2
-									{ 'd', 'e', 'f' },	// 3
-									{ 'g', 'h', 'f' },	// 4
-									{ 'j', 'k', 'l' },	// 5
-									{ 'm', 'n', 'o' },	// 6
-									{ 'p', 'r', 's' },	// 7
-									{ 't', 'u', 'v' },	// 8
-									{ 'w', 'x', 'y' }	// 9
-								  };
-	
-	
+	private static char[][] keyMap = { { '0' }, // 0
+			{ '1' }, // 1
+			{ 'a', 'b', 'c' }, // 2
+			{ 'd', 'e', 'f' }, // 3
+			{ 'g', 'h', 'f' }, // 4
+			{ 'j', 'k', 'l' }, // 5
+			{ 'm', 'n', 'o' }, // 6
+			{ 'p', 'r', 's' }, // 7
+			{ 't', 'u', 'v' }, // 8
+			{ 'w', 'x', 'y' } // 9
+	};
+
 	
 	
 	
@@ -65,18 +66,18 @@ public class TelephoneWords {
 		} else {
 			String head = input.substring(0, 1);
 			String tail = input.substring(1);
-			// recursive call may be outside loop since all sub-trees are identical 
+			// recursive call may be outside loop since all sub-trees are
+			// identical
 			List<String> tailTrans = translationsBottomUp(tail);
 			for (char headTran : keyMap[Integer.parseInt(head)]) {
 				for (String tailTran : tailTrans) {
 					combs.add(headTran + tailTran);
-				}	
+				}
 			}
 		}
 		return combs;
 	}
-	
-	
+
 	
 	
 	
@@ -86,8 +87,9 @@ public class TelephoneWords {
 		translationsTopDown(input, "", combs);
 		return combs;
 	}
-	
-	private static void translationsTopDown(String input, String output, List<String> acc) {
+
+	private static void translationsTopDown(String input, String output,
+			List<String> acc) {
 		if (input.length() == 0) {
 			// base case
 			acc.add(output);
